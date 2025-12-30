7795fac8785db202bfa49f2eb39443c949533e57
Gurpreet <gurpreet@users.noreply.github.com>
2025-12-30 17:31:51 -0500

    added analytics

---
+++ header.js
@@ -0,0 +1,21 @@
+// Google Analytics (G-1MFJXNN469)
+(function() {
+    var gaScript = document.createElement('script');
+    gaScript.async = true;
+    gaScript.src = 'https://www.googletagmanager.com/gtag/js?id=G-1MFJXNN469';
+    document.head.appendChild(gaScript);
+
+    window.dataLayer = window.dataLayer || [];
+    window.gtag = function(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+    gtag('config', 'G-1MFJXNN469');
+})();
+
+// Google AdSense (ca-pub-5461553569029344)
+(function() {
+    var adsScript = document.createElement('script');
+    adsScript.async = true;
+    adsScript.src = 'https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5461553569029344';
+    adsScript.crossOrigin = 'anonymous';
+    document.head.appendChild(adsScript);
+})();
